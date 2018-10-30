e54396df132834c6930c3c4833f1e463c9837cd2
Aurea <fu.sakura@gmail.com>
2018-10-30 12:25:26 -0700

    set up API code

--- seven_wonders.rb
+++ seven_wonders.rb
@@ -1,13 +1,66 @@
 require 'httparty'
+require_relative 'keys'
 
 #Starter Code:
-seven_wonders = ["Great Pyramid of Giza", "Hanging Gardens of Babylon", "Colossus of Rhodes", "Pharos of Alexandria", "Statue of Zeus at Olympia", "Temple of Artemis", "Mausoleum at Halicarnassus"]
+SEVEN_WONDERS = ["Great Pyramid of Giza", "Hanging Gardens of Babylon", "Colossus of Rhodes", "Pharos of Alexandria", "Statue of Zeus at Olympia", "Temple of Artemis", "Mausoleum at Halicarnassus"]
+
+class CoordinateFinder
+  attr_reader :location
+
+  def initialize(location)
+    @location = location
+  end
+
+  def get_coordinates
+    encoded_url = build_url
+
+    response = HTTParty.get(encoded_url)
+
+    return handle_response(response)
+  end
+
+  private
+
+  def build_url
+
+    address = @location.split.join('+')
+    coordinate_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&bounds=27.057438, 14.611095|40.948759, 49.189896&key=#{API_KEY}"
+    return URI.encode(coordinate_url)
+
+  end
+
+  def handle_response(response)
+
+    if response["status"] == "OK"
+
+      location = response["results"][0]["geometry"]["location"]
+
+      return {
+        "lat" => location["lat"],
+        "lng" => location["lng"]
+        }
+
+    else
+      return {
+        "lat" => "N/A",
+        "lng" => "N/A"
+        }
+    end
+  end
+end
+
+coordinate_hash = {}
+SEVEN_WONDERS.each do |wonder|
 
 
+  finder = CoordinateFinder.new(wonder)
+
+  coordinate_hash[finder.location] = finder.get_coordinates
 
 
+end
 
-
+puts coordinate_hash
 
 
 
